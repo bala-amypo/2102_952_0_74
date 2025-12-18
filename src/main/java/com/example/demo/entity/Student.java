fdf6b0d607afd356c31bfa1c55616ed59c87efd0
bala-amypo <bala@amypo.in>
2025-12-18 09:57:38 +0000

    User commit

--- src/main/java/com/example/demo/entity/Student.java
+++ src/main/java/com/example/demo/entity/Student.java
@@ -1,50 +1,64 @@
 package com.example.demo.entity;
 
-import java.time.*;
+import java.time.LocalDate;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+
 @Entity
-public class Student{
+@Table(name="Student")
+public class Student {
     @Id
-    //PRIMARY ID DECLARE PANNA
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
-    //TO AUTO GENERATE THE PRIMARY KEY
-    @NotNull
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotBlank(message = "Name is mandatory")
     private String name;
+    @Column(name="department")
+    @NotNull(message = "Department cannot be null")
     private String dept;
+    @PastOrPresent(message = "Date of Birth must be in the past or present")
     private LocalDate dob;
     private float cgpa;
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
+
+    public void setId(Long id) { 
         this.id = id;
-    }
-    public String getName() {
-        return name;
     }
     public void setName(String name) {
         this.name = name;
     }
-    public String getDept() {
-        return dept;
-    }
     public void setDept(String dept) {
         this.dept = dept;
-    }
-    public LocalDate getDob() {
-        return dob;
     }
     public void setDob(LocalDate dob) {
         this.dob = dob;
     }
-    public float getCgpa() {
-        return cgpa;
-    }
     public void setCgpa(float cgpa) {
         this.cgpa = cgpa;
     }
+
+    public Long getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getDept() {
+        return dept;
+    }
+    public LocalDate getDob() {
+        return dob;
+    }
+    public float getCgpa() {
+        return cgpa;
+    }
+    
     public Student(Long id, String name, String dept, LocalDate dob, float cgpa) {
         this.id = id;
         this.name = name;
@@ -52,7 +66,7 @@ public class Student{
         this.dob = dob;
         this.cgpa = cgpa;
     }
+
     public Student() {
     }
-    
 }
